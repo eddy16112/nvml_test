e8e10f894534fd831f957339cd8fca0a2da954af
Wei Wu <eddy16112@gmail.com>
2026-04-07 11:50:31 -0700

    check

--- pal.cpp
+++ pal.cpp
@@ -121,7 +121,7 @@ std::vector<ProcessorInfo> CudaPAL::enumerateProcessors() {
 
             nvmlC2cModeInfo_v1_t c2cInfo{};
             gpuInfo.hasC2C = (nvmlDeviceGetC2cModeInfoV(hDev, &c2cInfo) == NVML_SUCCESS
-                              && c2cInfo.isC2cEnabled) ? 1 : 0;
+                              && c2cInfo.isC2cEnabled);
 
             int lcnt = 0;
             for (unsigned l = 0; l < (unsigned)MAX_LINKS; l++) {
